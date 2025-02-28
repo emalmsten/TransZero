297db597b5eb8b36f215101d31a7e55e64a53d74
emil <e.l.malmsten@student.tudelft.nl>
2025-02-28 10:55:34 +0100

    runscript runpod

---
+++ rp_utils/rp_run.sh
@@ -0,0 +1,28 @@
+
+# install miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all
+
+# refresh bashrc
+source ~/.bashrc
+
+# make conda env called tz
+conda create -n tz python=3.10 -y
+conda activate tz
+
+# for lunarlander
+apt install swig -y
+
+# install dependencies
+#conda env create -f environment.yml
+conda install numpy==1.26.4 gymnasium pytorch torchvision torchaudio pytorch-cuda=12.1 nevergrad=1.0.1 bayesian-optimization=1.2.0 -c pytorch -c nvidia -c conda-forge -y
+conda install -c conda-forge "ray-default" -y
+pip install nevergrad wandb box2d-py minigrid
+
+# lunarlander
+cd TransZero
+python muzero.py -rfc rp -game "custom_grid"
