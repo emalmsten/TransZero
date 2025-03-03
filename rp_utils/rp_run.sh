5fc253c6a47d32d6daca1dd25afbc4562bcb3eeb
emil <e.l.malmsten@student.tudelft.nl>
2025-03-03 14:53:28 +0100

    small updates

--- rp_utils/rp_run.sh
+++ rp_utils/rp_run.sh
@@ -22,9 +22,8 @@ apt install swig -y
 conda install numpy==1.26.4 gymnasium pytorch torchvision torchaudio pytorch-cuda=12.1 nevergrad=1.0.1 bayesian-optimization=1.2.0 -c pytorch -c nvidia -c conda-forge -y
 conda install -c conda-forge "ray-default" box2d-py -y
 pip install nevergrad wandb minigrid
+pip install --upgrade ray
 
 # lunarlander
 # npm?
-cd ~
-cd TransZero
 python muzero.py -rfc rp -game "custom_grid"
